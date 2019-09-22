63c1c6160f89b35b74735062d972ce6dffa1e17e
Osman Akar <oakar.elk@gmail.com>
2019-09-22 22:50:38 +0300

    Prop types added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from '@/styles/app.module';
 
-const App = () => (
+const App = (props) => (
   <div>
-    <h1 data-test="test attr" className={styles.red}>Hello React</h1>
+    <h1 data-test="test attr" className={styles.red}>Hello React, {props.test}</h1>
   </div>
 );
 
 export default App;
+
+App.propTypes = {
+  test: PropTypes.string,
+}
